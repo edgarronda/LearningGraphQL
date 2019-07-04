a90e710bad370ca931d0c211931bead8aa15531f
Ed Ronda <edgar.g.ronda@gmail.com>
2019-07-04 10:44:33 -0700

    Separation of concerns, Resolvers.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema';
+import resolvers from './resolvers'
 import { getEnabledCategories } from 'trace_events';
 
 const app = express();
@@ -9,44 +10,7 @@ app.get('/', (req, res) => {
     res.send('GraphQL test!');
 });
 
-//Uses for new Friend creation.
-class Friend{
-    constructor(id, {firstName, lastName, gender, language, email }){
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.language = language;
-        this.email = email;
-    }
-}
-
-const friendDatabase = {};
-
-//Return the function(resolver) 
-const root = { 
-    friend: () => {
-        return{
-            "id": 11111,
-            "firstName": "Edgar",
-            "lastName": "Ronda",
-            "gender": "Male",
-            "language": "English",
-            "emails": [
-                {email: "me@mail.com"},
-                {email: "me2@mail.com"}
-            ],
-        }
-    },
-
-    //Friend Resolver.
-    createFriend: ({input}) => {
-        let id = require('crypto').randomBytes(10).toString('hex');
-        friendDatabase[id] = input;
-        return new Friend(id, input);
-    }
-
-};
+const root = resolvers;
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
